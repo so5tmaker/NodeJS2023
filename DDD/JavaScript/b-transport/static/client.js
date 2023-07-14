4401028885898c420fe34444a00dd2583c586188
Viktor Sivulskiy <info@softmaker.kz>
2023-07-14 07:50:05 +0600

    b-transport

--- DDD/JavaScript/b-transport/static/client.js
+++ DDD/JavaScript/b-transport/static/client.js
@@ -1,17 +1,49 @@
 'use strict';
 
-const socket = new WebSocket('ws://127.0.0.1:8001/');
 
-const scaffold = (structure) => {
+
+const transport = {};
+
+transport.http = (url) => (structure) => {
   const api = {};
   const services = Object.keys(structure);
-  for (const serviceName of services) {
-    api[serviceName] = {};
-    const service = structure[serviceName];
+  for (const name of services) {
+    api[name] = {};
+    const service = structure[name];
     const methods = Object.keys(service);
-    for (const methodName of methods) {
-      api[serviceName][methodName] = (...args) => new Promise((resolve) => {
-        const packet = { name: serviceName, method: methodName, args };
+    for (const method of methods) {
+      api[name][method] = (...args) => new Promise((resolve, reject) => {
+        // const url = `/api/${method}`;
+        console.log(url, args);
+        fetch(url, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(args),
+        }).then((res) => {
+          const { status } = res;
+          if (status !== 200) {
+            reject(new Error(`Status Code: ${status}`));
+            return;
+          }
+          resolve(res.json());
+        });
+      });
+    }
+  }
+  return api;
+};
+
+transport.ws = (url) => (structure) => {
+  const socket = new WebSocket(url); // 'ws://127.0.0.1:8001/'
+  const api = {};
+  const services = Object.keys(structure);
+  for (const name of services) {
+    api[name] = {};
+    const service = structure[name];
+    const methods = Object.keys(service);
+    for (const method of methods) {
+      api[name][method] = (...args) => new Promise((resolve) => {
+        const packet = { name: name, method: method, args };
         socket.send(JSON.stringify(packet));
         socket.onmessage = (event) => {
           const data = JSON.parse(event.data);
